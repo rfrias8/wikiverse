b30fb7bc5a801ac502033951ce5f1ee8bfc78e7e
rfrias8 <rfrias8@gmail.com>
2022-09-14 14:58:29 -0500

    Added form

---
+++ public/react/components/Form.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+
+export const Form = ()  => {
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [authorName, setAuthorName] = useState("");
+    const [email, setEmail] = useState("");
+    // const [tags, setTags] = ("")
+}
+
