dfc8b7aaf0dd9ef96216f89f63a48ed469d492f8
rfrias8 <rfrias8@gmail.com>
2022-09-14 14:58:19 -0500

    Added pagecomp and pagesList

--- public/react/components/PagesList.js
+++ public/react/components/PagesList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Page } from './Page';
 
-export const PagesList = ({pages}) => {
+export const PagesList = ({pages, fetchPage}) => {
+	console.log(pages);
 	return <>
 		{
 			pages.map((page, idx) => {
-				return <Page page={page} key={idx} />
+				return <Page page={page} key={idx} fetchPage={fetchPage} />
 			})
 		}
 	</>
